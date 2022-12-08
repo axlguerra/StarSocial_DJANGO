d3a730f408258c423d5c97deee851c859a631549
axlguerra <83897180+axlguerra@users.noreply.github.com>
2022-12-08 12:47:40 -0500

    Update settings.py

--- starsocial/starsocial/settings.py
+++ starsocial/starsocial/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_qb+xs%cuq3*1*fc8b24x!9ix=m6r3@l(ckdmsa4ooo!om66m6'
+SECRET_KEY =""
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
